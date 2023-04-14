d94c854154f0efc913d8728e8cb8442a58666aed
Mandresy-Andri <andriamasinoromandresy@gmail.com>
2023-04-14 13:32:55 -0400

    Cors try

--- src/main/java/com/company/TaskManager/controllers/EventController.java
+++ src/main/java/com/company/TaskManager/controllers/EventController.java
@@ -11,7 +11,7 @@ import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:3000") //allow CORS for my localhost
+@CrossOrigin(origins = "https://schedule-app-frontend.onrender.com") //allow CORS for my react client
 @RestController
 public class EventController {
 
